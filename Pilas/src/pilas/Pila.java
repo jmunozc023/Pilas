a1f325b03215b71ca6f7fe19f69fd07abf5cb0d2
jmunozc023 <jmunozc023@ulacit.ed.cr>
2023-06-01 19:23:37 -0600

    V1.0

---
+++ Pilas/src/pilas/Pila.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pilas;
+
+/**
+ *
+ * @author josep
+ */
+public class Pila {
+    private int cima;
+    private int vectorPila[];
+    public Pila(int tamano){
+        vectorPila= new int[tamano];
+        cima=0;
+    }
+    /*public void inicializarPila(int tamano){
+        vectorPila=new int[tamano];
+    }*/
+    public void push(int dato){
+        vectorPila[cima]=dato;
+        cima++;
+    }
+    public int pop(){
+        int eliminar =0;
+        if (cima==0) {
+            System.out.println("pila vacia");
+        } else {
+            eliminar = vectorPila[cima-1];
+            cima--;
+        }
+        return eliminar;
+    }
+    public int tamano(){
+        return vectorPila.length;
+    }
+    public boolean pilaVacia(){
+        if (cima==0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean pilaLlena(int tamano){
+        if (cima==tamano) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void mostrarPila(){
+        while (pilaVacia()==false) {
+            int aux= pop();
+            System.out.println(aux);
+        }
+    }
+}
---
+++ Pilas/src/pilas/Pilas.java
@@ -0,0 +1,21 @@
+package pilas;
+
+/**
+ *
+ * @author josep
+ */
+public class Pilas {
+
+    public static void main(String[] args) {
+        Pila pila = new Pila(4);
+        pila.push(8);
+        pila.push(9);
+        /*System.out.println("el valor extraido es: " + pila.pop());*/
+        pila.push(7);
+        pila.push(6);
+        /*System.out.println("el segundo valor extraido es: " + pila.pop());*/
+        pila.mostrarPila();
+
+    }
+
+}
